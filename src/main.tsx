7df03b95f679f71e9a88df7d202a4c9108500b0e
Thien Cao <caongocthien36@gmail.com>
2023-04-23 23:35:38 +0700

    create - setup redux

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css'
+import { store } from './store.ts'
+import { Provider } from 'react-redux'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 )
