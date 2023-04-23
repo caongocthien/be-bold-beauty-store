240484f158bc7c5083e0ff8775e8bd6594dd9a3e
Thien Cao <caongocthien36@gmail.com>
2023-04-23 23:30:16 +0700

    create - utils local storage func

---
+++ src/utils/utils.ts
@@ -0,0 +1,11 @@
+export const saveJwtToLocalStorage = (jwt: string) => {
+  return localStorage.setItem('jwt', jwt)
+}
+
+export const removeJwtToLocalStorage = () => {
+  return localStorage.removeItem('jwt')
+}
+
+export const getJwtToLocalStorage = () => {
+  return localStorage.getItem('jwt') || ''
+}
